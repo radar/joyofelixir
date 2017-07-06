4e775b422b807b62921797c71a4ac30712e395a7
Joy of Elixir <git@joyofelixir.com>
2017-07-06 19:15:05 +1000

    Shuffle things about

--- sync.sh
+++ sync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 bundle exec jekyll build
 aws s3 sync _site s3://joyofelixir.com --acl public-read
+# Invalid CloudFront caches, otherwise new content will not display.
+aws cloudfront create-invalidation --distribution-id E2VMVK9PHS2KRO --paths '/*
