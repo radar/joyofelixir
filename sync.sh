b861b23489af9d215b938be8bc3e3f02f31e4387
Ryan Bigg <git@ryanbigg.com>
2017-07-26 08:38:16 +1000

    Responsiveness is _hard_

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bundle exec jekyll build
-aws s3 sync _site s3://joyofelixir.com --acl public-read --delete
+aws s3 sync _site s3://joyofelixir.com --acl public-read --delete --exclude _site/book.pdf --exclude _site/images/*.pxm
 
 # Invalid CloudFront caches, otherwise new content will not display.
 
